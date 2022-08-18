e4f327d36c78ff08e59e9fef624f229b77a3ef7d
Replit user <>
2022-08-18 09:14:19 +0100

    more changes

--- index.js
+++ index.js
@@ -5,9 +5,16 @@ const bodyParser=require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 app.use(bodyParser.urlencoded({extended: false}));
-mongoose.connect("mongodb+srv://yylouay:h6GS@2tB2@bW3BW@cluster0.gh0basm.mongodb.net/userss?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://yylouay:h6GS@2tB2@bW3BW@cluster0.gh0basm.mongodb.net/users?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 let userSchema=mongoose.Schema({username:String});
 let User=mongoose.model("User",userSchema);
+let exerciseSchema=mongoose.Schema({
+  _id:String,
+  description:{type:String,required:true},
+  duration:{type:Number,required:true},
+  date:String
+})
+let Exercise=mongoose.model("Exercise",exerciseSchema);
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
@@ -20,22 +27,18 @@ app.post('/api/users',(req,res)=>
   user.save((err,use)=>{
     res.json({"_id":use._id,"username":use.username});
   })
-})
+});
 app.get('/api/users',(req,res)=>
 {
   User.find((err,array)=>
   {
-    if(err)
-      res.json({"error":"no users probably"});
-    else
-      res.send(array);
+    res.send(array);
   });
 
 });
 app.post('/api/users/:_id/exercises',(req,res)=>
   {
-    console.log("start");
-    let id=req.body._id;
+    let id=req._id;
     let description=req.body.description;
     let duration=req.body.duration;
     let date=req.body.date;
@@ -47,18 +50,21 @@ app.post('/api/users/:_id/exercises',(req,res)=>
     if ((description)&&(duration))
     User.findById(id,(err,user)=>
     {
+      if (err)
+        console.log("error 1");
       if (!err)
       {
-        var exercise=new Exercise({"_id":id,"description":desciption,
-                                "duration":duration,"date":date});
+        var exercise=new Exercise({"_id":id,"description":description,
+        "duration":duration,"date":date});
       exercise.save((err,ex)=>
         {
+          if (err)
+            console.log("error 2");
           if(!err)         res.json({"_id":user._id,"username":user.username,         "description":ex.description,"duration":ex.duration,"date":ex.date});
         }
       );
     }});
       });
-
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
